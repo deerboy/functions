dac249174e9725f9013ceae9251e9d34e1afed21
Daichi Ninomiya <daichi.ninomiya@deer.co.jp>
2022-01-29 15:59:36 +0900

    add create customer

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,26 @@
 import * as functions from "firebase-functions";
-
 import Stripe from "stripe";
+import * as admin from "firebase-admin";
+
+admin.initializeApp();
+
+const db = admin.firestore();
+const fns = functions.region("asia-northeast1");
 
 const stripe = new Stripe(functions.config().stripe.key, {
   apiVersion: "2020-08-27",
 });
 
-// // Start writing Firebase Functions
-// // https://firebase.google.com/docs/functions/typescript
-//
-// export const helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+export const createCustomer = fns.auth.user().onCreate(async (user) => {
+  const customer = await stripe.customers.create({
+    name: user.displayName,
+    email: user.email,
+    metadata: {
+      firebaseUID: user.uid,
+    },
+  });
+
+  return db.doc(`customers/${user.uid}`).set({
+    stripeId: customer.id,
+  });
+});
