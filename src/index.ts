29015f944859a15e66a51339d6c1a2eadb347aff
Daichi Ninomiya <daichi.ninomiya@deer.co.jp>
2022-01-29 17:31:26 +0900

    add create stripe account

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,7 @@ import * as admin from "firebase-admin";
 admin.initializeApp();
 
 const db = admin.firestore();
+const auth = admin.auth();
 const fns = functions.region("asia-northeast1");
 
 const stripe = new Stripe(functions.config().stripe.key, {
@@ -22,5 +23,27 @@ export const createCustomer = fns.auth.user().onCreate(async (user) => {
 
   return db.doc(`customers/${user.uid}`).set({
     stripeId: customer.id,
+  });
+});
+
+export const createStripeAccount = fns.https.onCall(async (_, context) => {
+  if (!context.auth) {
+    throw new functions.https.HttpsError("unauthenticated", "ログインが必要です");
+  }
+
+  const user = await auth.getUser(context.auth.uid);
+
+  const account = await stripe.accounts.create({
+    type: "express",
+    country: "JP",
+    email: user.email,
+    business_type: "individual",
+    company: {
+      name: user.displayName,
+    },
+  });
+
+  return db.doc(`stripeAccounts/${user.uid}`).set({
+    stripeAccountId: account.id,
   });
 });
